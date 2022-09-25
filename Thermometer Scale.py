be9f98ac4b1fea05dfdced854ba1d9a0a5a9f9e7
Roshae Sinclair <114318452+shaecodes@users.noreply.github.com>
2022-09-25 15:13:10 -0500

    Add files via upload

--- Thermometer Scale.py
+++ Thermometer Scale.py
@@ -4,6 +4,11 @@ def submit():
     print("The temperature is: " + str(scale.get()) + " degrees Celsius")
     
 window = Tk()
+window.title("Thermometer")
+
+icon = PhotoImage(file= 'thermometer.png')
+window.iconphoto(True,icon)
+window.geometry("500x800")
 
 hotImage = PhotoImage(file = 'fire.png')
 hotLabel = Label(image=hotImage)
@@ -17,9 +22,10 @@ scale = Scale(window,
         font_ = ('Consolas', 20),
         tickinterval = 10, #adds numeric indicators for value
         #resolution = 5 increment of slide
-        troughcolor ='#FFDEAD', #color of slider surface
+        troughcolor ='#F5E4D3', #color of slider surface
         fg = 'black',
-        bg= 'white'
+        bg= 'white',
+        showvalue = 0 #hide current value
 
         )
 scale.set(150) #sets the location of slider
